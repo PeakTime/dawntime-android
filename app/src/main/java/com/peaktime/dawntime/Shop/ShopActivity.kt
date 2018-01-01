46a0cc8ce0a8cf18c82ad0dd1f7b8e7a4c215ece
dmsal325 <dmsal7325@naver.com>
2018-01-01 15:54:24 +0900

    Shop 레이아웃 틀 완성

--- app/src/main/java/com/peaktime/dawntime/Shop/ShopActivity.kt
+++ app/src/main/java/com/peaktime/dawntime/Shop/ShopActivity.kt
@@ -1,13 +1,13 @@
 package com.peaktime.dawntime.Shop
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.Toast
 import com.peaktime.dawntime.R
+import kotlinx.android.synthetic.main.activity_shop.*
 
 class ShopActivity : AppCompatActivity() , View.OnClickListener{
 
@@ -20,7 +20,7 @@ class ShopActivity : AppCompatActivity() , View.OnClickListener{
         setContentView(R.layout.activity_shop)
 
 
-        shopList = findViewById(R.id.shop_list)
+        shopList = findViewById(R.id.shopList)
         shopList!!.layoutManager = GridLayoutManager(this, 2)
 
         shopDatas = ArrayList<ShopData>()
@@ -32,19 +32,40 @@ class ShopActivity : AppCompatActivity() , View.OnClickListener{
         shopDatas!!.add(ShopData(R.drawable.pika, "피카츄", "10000원"))
 
 
-
         shopAdapter = ShopAdapter(shopDatas)
         shopAdapter!!.setOnItemClickListener(this)
 
         shopList!!.adapter = shopAdapter
+
+
+        shopSearchBtn!!.setOnClickListener(ClickListener)
+
+    }
+
+    override fun onClick(v : View?) {
+
+        var intent = Intent(applicationContext, ShopDetailActivity::class.java)
+
+        val idx : Int = shopList!!.getChildAdapterPosition(v) //position 받아오기
+        val name : String = shopDatas!!.get(idx).shopName //포지션에 위치하는 이름받아오기
+        //val price : String = shopDatas!!.get(idx).shopPrice
+
+        intent.putExtra("name", name)
+        startActivity(intent)
+//        Toast.makeText(this, name, Toast.LENGTH_LONG).show()
+
     }
 
 
-    override fun onClick(v : View?) {
-        val idx : Int = shopList!!.getChildAdapterPosition(v) //position 받아오기
-        val name : String = shopDatas!!.get(idx).shopName //포지션에 위치하는 이름받아오기
-        val price : String = shopDatas!!.get(idx).shopPrice
+    private var ClickListener = View.OnClickListener { v ->
+        when (v.id) {
 
-        Toast.makeText(this, name, Toast.LENGTH_LONG).show()
+            R.id.shopSearchBtn -> {
+                var intent = Intent(applicationContext, ShopSearchActivity::class.java)
+                 startActivity(intent)
+            }
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/peaktime/dawntime/Shop/ShopSearchActivity.kt
@@ -0,0 +1,13 @@
+package com.peaktime.dawntime.Shop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.peaktime.dawntime.R
+
+class ShopSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_search)
+    }
+}
