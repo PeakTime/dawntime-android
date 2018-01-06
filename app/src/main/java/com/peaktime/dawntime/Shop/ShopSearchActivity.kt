c0fdeeba423c8284da39236f0de26f6ff54e2db1
dmsal325 <dmsal7325@naver.com>
2018-01-06 21:17:29 +0900

    Shop design + floating button

--- app/src/main/java/com/peaktime/dawntime/Shop/ShopSearchActivity.kt
+++ app/src/main/java/com/peaktime/dawntime/Shop/ShopSearchActivity.kt
@@ -3,7 +3,9 @@ package com.peaktime.dawntime.Shop
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.widget.TextView
 import com.peaktime.dawntime.R
 import kotlinx.android.synthetic.main.activity_shop_search.*
 
@@ -14,6 +16,20 @@ class ShopSearchActivity : AppCompatActivity() , View.OnClickListener{
         setContentView(R.layout.activity_shop_search)
 
         shopExitBtn!!.setOnClickListener(this)
+
+//        shopSearchEditText.setOnEditorActionListener(object : TextView.OnEditorActionListener {
+//            override fun onEditorAction(v: TextView, actionId: Int, event: KeyEvent): Boolean {
+//                when (actionId) {
+//                    EditorInfo.IME_ACTION_SEARCH -> {
+//                    }
+//                    else ->
+//                        // 기본 엔터키 동작
+//                        return false
+//                }// 검색 동작
+//                return true
+//            }
+//        })
+
     }
 
 
